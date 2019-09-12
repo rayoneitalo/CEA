6ed30b5c4efd92936ea1c3ae208ce0b6373daa1a
ItaloRayone <rayoneitalo@gmail.com>
2019-09-12 16:25:12 -0300

    Configurações de layaut na div map

--- src/main/webapp/js/spaceUser.js
+++ src/main/webapp/js/spaceUser.js
@@ -3,17 +3,16 @@ let divContainerElement = document.querySelector('.container');
 let divElementMap = document.querySelector('.map');
 let btnMenuElement = document.querySelector('button[id=btn-menu]');
 
-
 // Criando elementos na DOM
 let divElement = document.createElement('div');
-
 let btnElementClose = document.createElement('button');
-let txtElement = document.createTextNode('Fechar');
+
 
 // Setando atributos para os elementos criados
 divElement.setAttribute('class', 'map');
 btnElementClose.setAttribute('class', 'btn-menu');
-btnElementClose.appendChild(txtElement);
+
+
 
 
 
@@ -27,8 +26,6 @@ btnMenuElement.onclick = reder;
 // função que renderiza a div map
 // TODO Essa função não está terminada
 function reder() {
-    divContainerElement.appendChild(divElement);
-
-
-    divElementMap.appendChild(btnElementClose); // ! Não estã funcionando
+    divElementMap.style.display = "grid";
+    divElementMap.style.visibility = "visible";
 }
